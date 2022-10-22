97b3d7b72d0fa432260e5f674f56ed6730676f9d
CJ Edgecombe <cjledgecombe@gmail.com>
2022-10-22 12:35:23 -0600

    Complete initializing the app with live data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,53 @@
-const { useState } = React;
+const { useState, useEffect } = React;
 
 import exampleVideoData from '../data/exampleVideoData.js';
 import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
+import searchYouTube from '../lib/searchYouTube.js';
+import Search from './Search.js';
+
+var place = {
+  id: {
+    videoId: ''
+  },
+  snippet: {
+    title: '',
+    description: ''
+  }
+};
+
 
 var App = () => {
 
-  const [videoListState, setVideoListState] = useState(exampleVideoData);
-  const [currentVideo, setNewVideo] = useState(exampleVideoData[0]);
+  const [videoListState, setVideoListState] = useState([]);
+  const [currentVideo, setNewVideo] = useState(place);
+
+  var getYouTubeVideos = function (query) {
+    searchYouTube(query, function (videos) {
+      // console.log(videos);
+      setVideoListState(videos);
+      setNewVideo(videos[0]);
+      //setNewVideo(videos.forEach(element => element));
+    });
+  };
+
+  useEffect(() => { getYouTubeVideos('aaahhh'); }, [videoListState]);
+
+  //note: setNewVideo to VideoListEntry? define new handler function that
+  //runs setNewVideo func for us
+  //note: search API, get the event and add
+
+  var handleVideo = function (video) {
+    setNewVideo(video);
+  };
+
 
   return (
     <div>
       <nav className="navbar">
         <div className="col-md-6 offset-md-3">
-          <div><h5><em>search</em> view goes here</h5></div>
+          {/* <div><h5><em>search</em> view goes here</h5></div> */}
+          <Search searchAPI={getYouTubeVideos}/>
         </div>
       </nav>
       <div className="row">
@@ -22,7 +56,7 @@ var App = () => {
           <VideoPlayer video={currentVideo} />
         </div>
         <div className="col-md-5">
-          <VideoList videos={exampleVideoData} setNewVideo={setNewVideo}/>
+          <VideoList videos={videoListState} handleVideo={handleVideo}/>
           {/* <div><h5><em>videoList</em> view goes here</h5></div> */}
         </div>
       </div>
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -3,7 +3,7 @@ import VideoListEntry from './VideoListEntry.js';
 var VideoList = (props) => (
   <div className="video-list">
     {props.videos.map(video =>
-      <VideoListEntry video={video} setNewVideo={props.setNewVideo}/>
+      <VideoListEntry video={video} handleVideo={props.handleVideo}/>
     )}
 
     {/* <div><h5><em>videoListEntry</em> view goes here</h5></div>
